54d1afff65565461d8b3d57ade8a8b9177b30940
vcokltfre <vcokltfre@gmail.com>
2023-03-10 11:59:08 +0000

    feat: add discord bot

---
+++ src/discord.rs
@@ -0,0 +1,48 @@
+use std::{env, error::Error, sync::Arc};
+use twilight_gateway::{Event, Intents, Shard, ShardId};
+use twilight_http::{request::channel::reaction::RequestReactionType, Client as HttpClient};
+
+use super::rate;
+
+pub async fn run() -> Result<(), Box<dyn Error + Send + Sync>> {
+    let token = env::var("TOKEN")?;
+
+    let intents = Intents::GUILD_MESSAGES | Intents::MESSAGE_CONTENT;
+    let mut shard = Shard::new(ShardId::ONE, token.clone(), intents);
+    let http = Arc::new(HttpClient::new(token.clone()));
+
+    loop {
+        let event = match shard.next_event().await {
+            Ok(event) => event,
+            Err(why) => {
+                println!("Error: {:?}", why);
+                continue;
+            }
+        };
+
+        tokio::spawn(handle(event, Arc::clone(&http)));
+    }
+}
+
+async fn handle(event: Event, http: Arc<HttpClient>) {
+    match event {
+        Event::MessageCreate(message) => {
+            let (score, _) = rate::rate(message.content.to_string());
+
+            if score == 12 {
+                let result = http
+                    .create_reaction(
+                        message.channel_id,
+                        message.id,
+                        &RequestReactionType::Unicode { name: "⭐" },
+                    )
+                    .await;
+
+                if let Err(why) = result {
+                    println!("Error: {:?}", why);
+                }
+            }
+        }
+        _ => {}
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,37 @@
-fn rate(line: String) -> (i32, Vec<String>) {
-    let mut score = 0;
-    let mut issues = Vec::new();
+use dotenv::dotenv;
 
-    for c in line.chars() {
-        match c {
-            'a' | 's' | 'd' | 'f' | 'g' => score += 1,
-            'h' | 'j' | 'k' | 'l' => score += 1,
-            _ => {
-                score -= 1;
-                issues.push(format!("Bad keymash character: '{}'", c));
-            }
-        }
-    }
-
-    let mut repeats = 0;
-    let mut last_char = ' ';
-    let mut last_repeat = ' ';
-
-    for c in line.chars() {
-        if c == last_char {
-            repeats += 1;
-            if c != last_repeat {
-                issues.push(format!("Repeated character: '{}'", c));
-            }
-            last_repeat = c;
-        }
-
-        last_char = c;
-    }
-
-    score -= repeats;
-
-    let negative_modifier: i32;
-    let line_len = line.len() as i32;
-
-    // Ideal keymash length is 12 characters, a grace of 1 character is given
-    // Don't question it, I don't make the rules.
-    if line_len > 13 {
-        negative_modifier = line_len - 12;
-        issues.push(format!("Keymash too long: {} characters", line_len));
-    } else if line_len < 11 {
-        negative_modifier = 12 - line_len;
-        issues.push(format!("Keymash too short: {} characters", line_len));
-    } else {
-        negative_modifier = 0;
-    }
-
-    score /= (negative_modifier / 3) + 1;
-
-    if score > 12 {
-        score = 12;
-    }
-
-    (score, issues)
-}
+mod discord;
+mod rate;
 
 fn main() {
+    let args = std::env::args().collect::<Vec<String>>();
+    if args.len() == 2 {
+        if args[1] == "discord" {
+            println!("Running in Discord mode...");
+            dotenv().ok();
+            let future = discord::run();
+            if let Err(why) = tokio::runtime::Builder::new_multi_thread()
+                .enable_all()
+                .build()
+                .unwrap()
+                .block_on(future)
+            {
+                println!("Error: {:?}", why);
+            }
+            return;
+        }
+
+        println!("Unknown argument: {}", args[1]);
+        return;
+    }
+
     loop {
         let mut line = String::new();
         std::io::stdin().read_line(&mut line).unwrap();
 
         line.pop();
 
-        let (score, issues) = rate(line);
+        let (score, issues) = rate::rate(line);
 
         let score_percentage = ((score as f32 / 12.0) * 100.0) as i32;
 
